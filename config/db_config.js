f859cd222ce53956a2b5ee79c86a80c4abcbfb32
aurumdev952 <benjamugi20072@gmail.com>
2022-11-12 22:35:42 +0200

    solved the database issue.

--- config/db_config.js
+++ config/db_config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-
-
+const atlas_url = "mongodb+srv://aurumdev:fidele95@cluster0.mnvdota.mongodb.net/?retryWrites=true&w=majority";
+const local_url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0');
+        const conn = await mongoose.connect(atlas_url, { useNewUrlParser: true, useUnifiedTopology: true });
         // const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`mongoDB connected to${conn.connection.host}`);
